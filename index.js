62c01991bf759cc593e3c5b2848671a182089f45
Alexander Dubois <sander.dubois@outlook.com>
2019-03-13 13:10:03 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf() {
+  const body = {
+    firstName: "Sander",
+    registryMessage: "Hi there human"
+  }
+  const params = {
+    method: "POST",
+    body: JSON.stringify(body)
+  }
+
+  return fetch('http://guestbook.example.com/register', params)
+      .then(response => response.json())
+      .then(json => json.message)
+}
+
+function errorSelf() {
+
+  const body = {
+    firstName: "Sander",
+    registryMessage: "Hi there human"
+  }
+  const params = {
+    method: "POST",
+    body: JSON.stringify(body)
+  }
+
+  return fetch('http://guestbook.example.com/register-error', params)
+      .then(response => response.json())
+      .catch(_ => ':(')
+}
