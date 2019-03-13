cc8f944380351645d4d36bf975fdbbe010313505
stubrew24 <sewelly@gmail.com>
2019-03-13 21:30:13 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf(){
+  return fetch('http://guestbook.example.com/register', {
+    "method": "POST",
+    "body": {
+      "firstName": "Stuart",
+      "registryMessage": "Something"
+    }
+  }).then(function(response){return response.json()}).then(function(json){return json.message})
+}
+
+
+function errorSelf(){
+  return fetch('http://guestbook.example.com/register-error', {
+    "method": "POST",
+    "body": {
+      "firstName": "Stuart",
+      "registryMessage": "Something"
+    }
+  }).then(function(response){return response.json()
+  }).then(function(json){return json.message
+  }).catch(function(error){
+    return ":("
+  })
+}
